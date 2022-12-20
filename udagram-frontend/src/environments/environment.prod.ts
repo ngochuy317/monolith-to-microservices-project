ca00e0c71f6cae88d0b3cb6f1949798bda71d0ff
HuyNguyen <ngochuy317@gmail.com>
2022-12-20 11:43:34 +0700

    update env

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a2a0672311a434026ba79feda3bd2406-1552719638.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
